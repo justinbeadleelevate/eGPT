de6cc44d7a3dbbb2ef686caf6dcfb52bb6c0f4d8
justinbeadleelevate <77413737+justinbeadleelevate@users.noreply.github.com>
2025-04-03 11:53:28 -0700

    MILLIONTH TRY

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,4 +1,4 @@
-export const AI_NAME = "CHTTY";
+export const AI_NAME = "CHATTY";
 export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
